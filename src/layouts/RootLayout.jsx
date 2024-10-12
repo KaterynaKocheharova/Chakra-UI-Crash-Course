43a72f9d7ae248e1d2133ce419a5487f2bcec56c
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-10-12 19:55:49 +0300

    add form

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -5,22 +5,20 @@ import Sidebar from "../Components/Sidebar";
 
 export default function RootLayout() {
   return (
-  
-      <Grid templateColumns="repeat(6, 1fr)" bg="gray.50">
-        <GridItem
-          as="aside"
-          colSpan={{ base: 6, lg: 2, xl: 1 }}
-          bg="purple.400"
-          p={{ base: "20px", lg: "30px" }}
-          minHeight={{ lg: "100vh" }}
-        >
-          <Sidebar/>
-        </GridItem>
-        <GridItem as="main" colSpan={{ base: 6, lg: 4, xl: 5 }} p="40px">
-          <NavBar />
-          <Outlet />
-        </GridItem>
-      </Grid>
-
+    <Grid templateColumns="repeat(6, 1fr)" bg="gray.50">
+      <GridItem
+        as="aside"
+        colSpan={{ base: 6, lg: 2, xl: 1 }}
+        bg="purple.400"
+        p={{ base: "20px", lg: "30px" }}
+        minHeight={{ lg: "100vh" }}
+      >
+        <Sidebar />
+      </GridItem>
+      <GridItem as="main" colSpan={{ base: 6, lg: 4, xl: 5 }} p="40px">
+        <NavBar />
+        <Outlet />
+      </GridItem>
+    </Grid>
   );
 }
--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,5 +1,53 @@
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  FormHelperText,
+  Textarea,
+  Checkbox,
+  Button,
+} from "@chakra-ui/react";
+import { Form, redirect } from "react-router-dom";
+
 export default function Create() {
   return (
-    <div>Create</div>
-  )
+    <Box>
+      <Form method="post" action="/create">
+        <FormControl maxW="480px" isRequired mb="40px">
+          <FormLabel>Task name:</FormLabel>
+          <Input type="text" name="title" />
+          <FormHelperText>Offer a descriptive task name</FormHelperText>
+        </FormControl>
+        <FormControl maxW="480px" mb="40px">
+          <FormLabel>Task description:</FormLabel>
+          <Textarea
+            placeholder="Enter a detailed description for the task"
+            name="description"
+          />
+        </FormControl>
+        <FormControl maxW="480px" display="flex" alignItems="center" mb="40px">
+          <Checkbox name="isPriority" size="lg" colorScheme="purple" />
+          <FormLabel mb="0" ml="10px">
+            Make this a priority task
+          </FormLabel>
+        </FormControl>
+        <Button type="submit">Submit</Button>
+      </Form>
+    </Box>
+  );
 }
+
+export const createAction = async ({ request }) => {
+  const data = await request.formData();
+
+  const task = {
+    title: data.get("title"),
+    description: data.get("description"),
+    isPriority: data.get("isPriority") === "",
+  };
+
+  console.log(task);
+
+  return redirect("/");
+};
